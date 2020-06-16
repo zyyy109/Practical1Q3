b0373202c848cc87ac9ab05fdaff0aa0eff4be58
TARUC <TARUC@PC-82>
2020-06-16 12:20:38 +0800

    more func

--- JavaApplication5/src/ValidatesCreditCard.java
+++ JavaApplication5/src/ValidatesCreditCard.java
@@ -6,6 +6,7 @@ public class ValidatesCreditCard {
         
         String cardNo = "";
         int sum1 = 0;
+        int sum2 = 0;
         
         Scanner scan = new Scanner(System.in);
         
@@ -15,8 +16,28 @@ public class ValidatesCreditCard {
         for(int i=7; i>0; i-=2 ){
            int temp =  cardNo.charAt(i) - '0';
            sum1 += temp;
-  
         }
-        System.out.print("\n sum :" + sum1);
+        
+        for(int i=6; i>=0; i-=2 ){
+           int temp =  cardNo.charAt(i) - '0';
+           
+           int doubling = temp * 2 ;
+           
+           if (doubling >= 10){
+               int q = doubling /10;
+               int r = doubling % 10;
+               
+                sum2 += q + r;
+           }else{
+               sum2 += doubling;
+           }
+        }
+         
+        if ( (sum1 + sum2) % 10 != 0){
+             System.out.print("\n Invalid card number:");
+        } else{
+            System.out.print("\n Valid card number:");
+        }
+    
     }
 }
