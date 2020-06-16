cd9e916a199a6041495ee75f7dd4b741352a82d5
TARUC <TARUC@PC-82>
2020-06-16 10:12:22 +0800

    more func

--- JavaApplication5/src/ValidatesCreditCard.java
+++ JavaApplication5/src/ValidatesCreditCard.java
@@ -1,5 +1,22 @@
-
+import java.util.Scanner;
 
 public class ValidatesCreditCard {
     
+    public static void main(String[] args) {
+        
+        String cardNo = "";
+        int sum1 = 0;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter 8-digit Card number :");
+        cardNo = scan.nextLine();
+        
+        for(int i=7; i>0; i-=2 ){
+           int temp =  cardNo.charAt(i) - '0';
+           sum1 += temp;
+  
+        }
+        System.out.print("\n sum :" + sum1);
+    }
 }
